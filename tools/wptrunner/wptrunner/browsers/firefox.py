c935d6d00edd6c893c17545e7c886fd5d3ce72c1
Wes Kocher <kwierso@gmail.com>
2019-01-23 15:24:52 -0800

    Bug 1520806 - Don't apply unittest-features prefs when running Firefox stable
    This skips over adding the unittest-features profile's user.js prefs if the browser channel is not Nightly. This should in theory mean the wpt run will only use preferences necessary for the tests to technically run, plus whatever the version of Firefox used for the tests includes as default out of the box prefs.

--- tools/wptrunner/wptrunner/browsers/firefox.py
+++ tools/wptrunner/wptrunner/browsers/firefox.py
@@ -86,6 +86,7 @@ def browser_kwargs(test_type, run_info_data, config, **kwargs):
             "stylo_threads": kwargs["stylo_threads"],
             "chaos_mode_flags": kwargs["chaos_mode_flags"],
             "config": config,
+            "browser_channel": kwargs["browser_channel"],
             "headless": kwargs["headless"]}
 
 
@@ -183,7 +184,7 @@ class FirefoxBrowser(Browser):
                  symbols_path=None, stackwalk_binary=None, certutil_binary=None,
                  ca_certificate_path=None, e10s=False, stackfix_dir=None,
                  binary_args=None, timeout_multiplier=None, leak_check=False, asan=False,
-                 stylo_threads=1, chaos_mode_flags=None, config=None, headless=None, **kwargs):
+                 stylo_threads=1, chaos_mode_flags=None, config=None, browser_channel=None, headless=None, **kwargs):
         Browser.__init__(self, logger)
         self.binary = binary
         self.prefs_root = prefs_root
@@ -217,6 +218,7 @@ class FirefoxBrowser(Browser):
         self.lsan_handler = None
         self.stylo_threads = stylo_threads
         self.chaos_mode_flags = chaos_mode_flags
+        self.browser_channel = browser_channel
         self.headless = headless
 
     def settings(self, test):
@@ -313,7 +315,10 @@ class FirefoxBrowser(Browser):
         if os.path.isfile(profiles):
             with open(profiles, 'r') as fh:
                 for name in json.load(fh)['web-platform-tests']:
-                    pref_paths.append(os.path.join(self.prefs_root, name, 'user.js'))
+                    if self.browser_channel == 'nightly':
+                        pref_paths.append(os.path.join(self.prefs_root, name, 'user.js'))
+                    elif name != 'unittest-features':
+                        pref_paths.append(os.path.join(self.prefs_root, name, 'user.js'))
         else:
             # Old preference files used before the creation of profiles.json (remove when no longer supported)
             legacy_pref_paths = (
